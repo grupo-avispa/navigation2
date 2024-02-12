a3220b728960cac10bf126c9f433835fd9a6f297
Alberto Tudela <ajtudela@gmail.com>
2024-02-12 12:33:11 +0100

    Changes for humble
    Signed-off-by: Alberto Tudela <ajtudela@gmail.com>

--- nav2_util/include/nav2_util/node_utils.hpp
+++ nav2_util/include/nav2_util/node_utils.hpp
@@ -160,6 +160,25 @@ std::string get_plugin_type_param(
  */
 void setSoftRealTimePriority();
 
+/**
+ * @brief A method to copy all parameters from one node (parent) to another (child).
+ * May throw parameter exceptions in error conditions
+ * @param parent Node to copy parameters from
+ * @param child Node to copy parameters to
+ */
+template<typename NodeT1, typename NodeT2>
+void copy_all_parameters(const NodeT1 & parent, const NodeT2 & child)
+{
+  using Parameters = std::vector<rclcpp::Parameter>;
+  std::vector<std::string> param_names = parent->list_parameters({}, 0).names;
+  Parameters params = parent->get_parameters(param_names);
+  for (Parameters::const_iterator iter = params.begin(); iter != params.end(); ++iter) {
+    if (!child->has_parameter(iter->get_name())) {
+      child->declare_parameter(iter->get_name(), iter->get_parameter_value());
+    }
+  }
+}
+
 }  // namespace nav2_util
 
 #endif  // NAV2_UTIL__NODE_UTILS_HPP_
